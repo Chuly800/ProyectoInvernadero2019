45e7ee29b2590290a0971467687587ac71c32c3d
julian medina <jmmedina45@misena.edu.co>
2020-06-01 10:46:43 -0500

    actualizacion

--- src/app/componentes/menu/menu.component.ts
+++ src/app/componentes/menu/menu.component.ts
@@ -1,21 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/servicios/auth.service';
 import { MenuController } from '@ionic/angular';
+import * as firebase from 'firebase';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
 
-  constructor( public authservice: AuthService, public menuCtrl: MenuController) { }
+  constructor( public authservice: AuthService, public menuCtrl: MenuController, private db: AngularFirestore) {
+  }
+  // getName() {
+  //   this.afd.list('usuarios/').valueChanges().subscribe(
+  //     data => {
+  //       console.log(data);
+  //       this.items = data;
+  //     }
+  //   );
+// const userId = firebase.auth().currentUser.uid;
 
   cerrarSesion() {
     this.authservice.logout();
     this.menuCtrl.close();
   }
+// return firebase.database().ref('usuarios'.userId).once('value').then(function(snapshot) {
+//     const username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+//   // ...
+// });
 
-  ngOnInit() {}
+
+
 
 }
---
+++ src/app/models/menu.ts
@@ -0,0 +1,8 @@
+export interface Menu {
+      name: string;
+      lastname: string;
+      identificacion: any;
+      email: string;
+      telefono: any;
+      password: string;
+}
